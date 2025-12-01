226600d4ac20bd84dff7a200414d0a81f7b312e0
Lukas Deward <44476930+lukasdeward@users.noreply.github.com>
2025-12-01 12:58:10 +0100

    t

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,5 +42,12 @@ export default defineNuxtConfig({
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_ANON_KEY || process.env.SUPABASE_KEY,
     serviceKey: process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_SERVICE_KEY
+  },
+
+  image: {
+    provider: 'ipx',
+    domains: [
+      process.env.SUPABASE_URL ? new URL(process.env.SUPABASE_URL).host : undefined
+    ].filter(Boolean) as string[]
   }
 })
